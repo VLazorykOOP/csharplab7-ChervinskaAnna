bc2c7a3f798a696d7db8e1d7768bc0aed8e73e9b
ChervinskaAnna <chervinska.anna@chnu.edu.ua>
2024-03-26 13:43:44 +0200

    task 1

--- Lab7CSharp/Form1.cs
+++ Lab7CSharp/Form1.cs
@@ -1,96 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
 using System.Windows.Forms;
 
-namespace DrawingApp
+namespace WindowsFormsApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        Random random = new Random();
-        List<Figure> figures = new List<Figure>(); // Оголошення списку для зберігання фігур
-
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void addButton_Click(object sender, EventArgs e)
         {
-            // Можливо, ініціалізувати об'єкти малюнка тут
-        }
-
-        private void btnDraw_Click(object sender, EventArgs e)
-        {
-            // Код для малювання об'єктів на PictureBox
-            Graphics g = pictureBox1.CreateGraphics();
-            // Цикл для малювання об'єктів з масиву
-            foreach (Figure figure in figures)
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
             {
-                figure.Draw(g);
+                comboBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
             }
-            g.Dispose();
+            else
+            {
+                MessageBox.Show("Enter text");
+            }
         }
 
-        // Код для додавання нового об'єкта до масиву
-        private void btnAddObject_Click(object sender, EventArgs e)
+        private void removeButton_Click(object sender, EventArgs e)
         {
-            // Отримати дані про об'єкт з полів форми та створити об'єкт
-            // Додати створений об'єкт до списку фігур
-        }
-
-        // Код для переміщення об'єкту за допомогою кнопки
-        private void btnMove_Click(object sender, EventArgs e)
-        {
-            // Викликати метод переміщення об'єкту з масиву
-        }
-    }
-
-    // Класи для фігур
-    public abstract class Figure
-    {
-        protected int x, y; // Координати фігури
-        protected Color color; // Колір фігури
-
-        public abstract void Draw(Graphics g); // Метод малювання
-
-        public virtual void Move(int dx, int dy) // Метод переміщення
-        {
-            x += dx;
-            y += dy;
-        }
-    }
-
-    // Класи конкретних фігур
-    public class Square : Figure
-    {
-        public override void Draw(Graphics g)
-        {
-            // Реалізація малювання квадрата
-        }
-    }
-
-    public class MyRectangle : Figure // Змінив назву класу Rectangle на MyRectangle, щоб уникнути конфлікту з іншим класом з такою ж назвою
-    {
-        public override void Draw(Graphics g)
-        {
-            // Реалізація малювання прямокутника
-        }
-    }
-
-    public class Ellipse : Figure
-    {
-        public override void Draw(Graphics g)
-        {
-            // Реалізація малювання еліпса
-        }
-    }
-
-    public class Rhombus : Figure
-    {
-        public override void Draw(Graphics g)
-        {
-            // Реалізація малювання ромба
+            if (comboBox1.SelectedIndex != -1)
+            {
+                comboBox1.Items.RemoveAt(comboBox1.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Select to remove");
+            }
         }
     }
 }
--- Lab7CSharp/Program.cs
+++ Lab7CSharp/Program.cs
@@ -1,23 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using DrawingApp;
 
-namespace Lab7CSharp
+namespace WindowsFormsApp
 {
     static class Program
     {
         /// <summary>
-        /// Главная точка входа для приложения.
+        /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
