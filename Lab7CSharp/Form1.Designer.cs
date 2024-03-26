f30b6c1c94cc3d716ade80fa68dfea10301f3645
ChervinskaAnna <chervinska.anna@chnu.edu.ua>
2024-03-26 16:41:37 +0200

    task 2

--- Lab7CSharp/Form1.Designer.cs
+++ Lab7CSharp/Form1.Designer.cs
@@ -32,28 +32,36 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.addButton = new System.Windows.Forms.Button();
             this.removeButton = new System.Windows.Forms.Button();
+            this.openFileDialogButton = new System.Windows.Forms.Button();
+            this.saveFileDialogButton = new System.Windows.Forms.Button();
+            this.convertToGrayScaleButton = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(34, 25);
+            this.comboBox1.Location = new System.Drawing.Point(51, 52);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(220, 21);
+            this.comboBox1.Size = new System.Drawing.Size(328, 28);
             this.comboBox1.TabIndex = 0;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(34, 68);
+            this.textBox1.Location = new System.Drawing.Point(51, 105);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 20);
+            this.textBox1.Size = new System.Drawing.Size(328, 26);
             this.textBox1.TabIndex = 1;
             // 
             // addButton
             // 
-            this.addButton.Location = new System.Drawing.Point(180, 68);
+            this.addButton.Location = new System.Drawing.Point(51, 168);
+            this.addButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.Size = new System.Drawing.Size(132, 50);
             this.addButton.TabIndex = 2;
             this.addButton.Text = "Add";
             this.addButton.UseVisualStyleBackColor = true;
@@ -61,25 +69,71 @@
             // 
             // removeButton
             // 
-            this.removeButton.Location = new System.Drawing.Point(34, 108);
+            this.removeButton.Location = new System.Drawing.Point(240, 168);
+            this.removeButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.removeButton.Name = "removeButton";
-            this.removeButton.Size = new System.Drawing.Size(75, 23);
+            this.removeButton.Size = new System.Drawing.Size(139, 50);
             this.removeButton.TabIndex = 3;
             this.removeButton.Text = "Remove";
             this.removeButton.UseVisualStyleBackColor = true;
             this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
             // 
+            // openFileDialogButton
+            // 
+            this.openFileDialogButton.Location = new System.Drawing.Point(24, 272);
+            this.openFileDialogButton.Name = "openFileDialogButton";
+            this.openFileDialogButton.Size = new System.Drawing.Size(107, 50);
+            this.openFileDialogButton.TabIndex = 4;
+            this.openFileDialogButton.Text = "Open";
+            this.openFileDialogButton.UseVisualStyleBackColor = true;
+            this.openFileDialogButton.Click += new System.EventHandler(this.openFileDialogButton_Click);
+            // 
+            // saveFileDialogButton
+            // 
+            this.saveFileDialogButton.Location = new System.Drawing.Point(150, 272);
+            this.saveFileDialogButton.Name = "saveFileDialogButton";
+            this.saveFileDialogButton.Size = new System.Drawing.Size(108, 50);
+            this.saveFileDialogButton.TabIndex = 5;
+            this.saveFileDialogButton.Text = "Save";
+            this.saveFileDialogButton.UseVisualStyleBackColor = true;
+            this.saveFileDialogButton.Click += new System.EventHandler(this.saveFileDialogButton_Click);
+            // 
+            // convertToGrayScaleButton
+            // 
+            this.convertToGrayScaleButton.Location = new System.Drawing.Point(276, 272);
+            this.convertToGrayScaleButton.Name = "convertToGrayScaleButton";
+            this.convertToGrayScaleButton.Size = new System.Drawing.Size(132, 50);
+            this.convertToGrayScaleButton.TabIndex = 6;
+            this.convertToGrayScaleButton.Text = "Grayscale";
+            this.convertToGrayScaleButton.UseVisualStyleBackColor = true;
+            this.convertToGrayScaleButton.Click += new System.EventHandler(this.convertToGrayScaleButton_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(414, 38);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(737, 476);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 161);
+            this.ClientSize = new System.Drawing.Size(1195, 553);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.convertToGrayScaleButton);
+            this.Controls.Add(this.saveFileDialogButton);
+            this.Controls.Add(this.openFileDialogButton);
             this.Controls.Add(this.removeButton);
             this.Controls.Add(this.addButton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "MainForm";
             this.Text = "MainForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -91,5 +145,9 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button addButton;
         private System.Windows.Forms.Button removeButton;
+        private System.Windows.Forms.Button openFileDialogButton;
+        private System.Windows.Forms.Button saveFileDialogButton;
+        private System.Windows.Forms.Button convertToGrayScaleButton;
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
--- Lab7CSharp/Form1.cs
+++ Lab7CSharp/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp
@@ -34,5 +35,81 @@ namespace WindowsFormsApp
                 MessageBox.Show("Select to remove");
             }
         }
+
+        private void openFileDialogButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Images|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            openFileDialog1.Title = "Select an Image";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox.Image = new Bitmap(openFileDialog1.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not open image: " + ex.Message);
+                }
+            }
+        }
+
+        private void saveFileDialogButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image == null)
+            {
+                MessageBox.Show("Open an image first.");
+                return;
+            }
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "Images|*.jpg;*.jpeg;*.png;*.bmp;*.gif";
+            saveFileDialog1.Title = "Save Image As";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    pictureBox.Image.Save(saveFileDialog1.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not save image: " + ex.Message);
+                }
+            }
+        }
+
+        private void convertToGrayScaleButton_Click(object sender, EventArgs e)
+        {
+            if (pictureBox.Image == null)
+            {
+                MessageBox.Show("Open an image first.");
+                return;
+            }
+
+            Bitmap originalImage = new Bitmap(pictureBox.Image);
+            Bitmap grayScaleImage = ToGrayScale(originalImage);
+            pictureBox.Image = grayScaleImage;
+        }
+
+        private Bitmap ToGrayScale(Bitmap originalImage)
+        {
+            Bitmap grayScaleImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    Color originalColor = originalImage.GetPixel(x, y);
+                    int grayScaleValue = (int)(originalColor.R * 0.3 + originalColor.G * 0.59 + originalColor.B * 0.11);
+                    Color grayScaleColor = Color.FromArgb(grayScaleValue, grayScaleValue, grayScaleValue);
+                    grayScaleImage.SetPixel(x, y, grayScaleColor);
+                }
+            }
+
+            return grayScaleImage;
+        }
+
     }
 }
